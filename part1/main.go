222fda6c4d3278e6eafd6548f929ac7a8ca55bf5
gsingharoy <neogauravsvnit@gmail.com>
2017-09-24 18:59:34 +0200

    Part1
    built the part1 of the tutorial which contains a basic project.

---
+++ part1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}

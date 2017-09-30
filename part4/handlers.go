50a4b7697ca0d272c3bd24bde637aa11c163270f
gsingharoy <neogauravsvnit@gmail.com>
2017-09-30 11:28:44 +0200

    Part4
    Created the basic part4 part of the tutorial.

---
+++ part4/handlers.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+// Handler for the books index action
+// GET /books
+func BookIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	books := []*Book{}
+	for _, book := range bookstore {
+		books = append(books, book)
+	}
+	response := &JsonResponse{Data: &books}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		panic(err)
+	}
+}
+
+// Handler for the books Show action
+// GET /books/:isdn
+func BookShow(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	isdn := params.ByName("isdn")
+	book, ok := bookstore[isdn]
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	if !ok {
+		// No book with the isdn in the url has been found
+		w.WriteHeader(http.StatusNotFound)
+		response := JsonErrorResponse{Error: &ApiError{Status: 404, Title: "Record Not Found"}}
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			panic(err)
+		}
+		return
+	}
+	response := JsonResponse{Data: book}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		panic(err)
+	}
+}
---
+++ part4/responses.go
@@ -0,0 +1,16 @@
+package main
+
+type JsonResponse struct {
+	// Reserved field to add some meta information to the API response
+	Meta interface{} `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+type JsonErrorResponse struct {
+	Error *ApiError `json:"error"`
+}
+
+type ApiError struct {
+	Status int16  `json:"status"`
+	Title  string `json:"title"`
+}
